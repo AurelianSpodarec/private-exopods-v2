dce7e30b0fa56b46bb77b4ee8b3e1a988374eb89
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-20 18:33:42 +0100

    feat: Added Aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react'
+import path from "path";
+
+const getDirName = function () {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    return __dirname;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+        assets: `${path.resolve(getDirName, "./src/assets/")}`,
+        config: `${path.resolve(getDirName, "./src/config/")}`,
+        styles: `${path.resolve(getDirName, "./src/styles/")}`,
+        
+        layout: `${path.resolve(getDirName, "./src/views/_layout/")}`,
+        atoms: `${path.resolve(getDirName, "./src/views/atoms/")}`,
+        molecules: `${path.resolve(getDirName, "./src/views/molecules/")}`,
+        pages: `${path.resolve(getDirName, "./src/views/pages/")}`,
+    }
+},
 })
