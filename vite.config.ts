84b632ac370e20b96731bc0057c6f5a9f985f79b
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-20 18:44:56 +0100

    fix: Fixed alias importing

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,24 @@ import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react'
 import path from "path";
 
-const getDirName = function () {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    return __dirname;
-}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-        assets: `${path.resolve(getDirName, "./src/assets/")}`,
-        config: `${path.resolve(getDirName, "./src/config/")}`,
-        styles: `${path.resolve(getDirName, "./src/styles/")}`,
-        
-        layout: `${path.resolve(getDirName, "./src/views/_layout/")}`,
-        atoms: `${path.resolve(getDirName, "./src/views/atoms/")}`,
-        molecules: `${path.resolve(getDirName, "./src/views/molecules/")}`,
-        pages: `${path.resolve(getDirName, "./src/views/pages/")}`,
-    }
-},
+    resolve: {
+        alias: {
+            assets: `${path.resolve(__dirname, "./src/assets/")}`,
+            config: `${path.resolve(__dirname, "./src/config/")}`,
+            styles: `${path.resolve(__dirname, "./src/styles/")}`,
+            
+            layout: `${path.resolve(__dirname, "./src/views/_layout/")}`,
+            atoms: `${path.resolve(__dirname, "./src/views/atoms/")}`,
+            molecules: `${path.resolve(__dirname, "./src/views/molecules/")}`,
+            pages: `${path.resolve(__dirname, "./src/views/pages/")}`,
+        }
+    },
 })
