73cf533f815fa01b5a3409fc2c9862a8554cb2dc
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-20 18:45:22 +0100

    feat: Added main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import App from './views/App';
+
+import './../src/styles/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
