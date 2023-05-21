f6520df82cadffce8614962a5b14669ddeeb7c11
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-21 15:42:40 +0100

    feat: Added container box and some styles for container create

--- src/views/pages/Services/ServicesCreate/ServicesCreate.tsx
+++ src/views/pages/Services/ServicesCreate/ServicesCreate.tsx
@@ -1,8 +1,49 @@
+import Container from "layout/Container";
+import PageHeader from "molecules/PageHeader";
+
+// TODO: Refactor once API ready
+function Box() {
+    return (
+        <div className="bg-[#F5F5F7] px-6 py-3 rounded-xl">
+        <div className="flex items-center">
+            <div className="bg-[#D7D7D7] rounded-full p-3 h-14 w-14">
+                <img src="https://i.postimg.cc/cJfGhj36/wordpress-svgrepo-com-1.png" alt="" />
+            </div>
+            <h3 className="text-xl text-[#0C0B0B] font-normal px-8">Jenkins</h3>
+        </div>
+        </div>
+    )
+}
+
 function ServicesCreate() {
     return (
-        <div>
-            Services create
-        </div>
+        <Container>
+
+                <section>
+                    <PageHeader title="Chose Container" />
+                    <div className="flex space-x-6">
+                        <Box />
+                        <Box />
+                        <Box />
+                    </div>
+                </section>
+
+                <div className="my-8">
+                    <span>Or</span>
+                </div>
+
+                <section>
+                    <PageHeader title="Create your first free container">
+                        <p className="mt-2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </PageHeader>
+                    <div>
+                        
+                    </div>
+                </section>
+               
+
+
+        </Container>
     )
 }
 
