7f0728b980cc3253c0d538cfab895a771803b81c
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-21 14:47:18 +0100

    fix: Fixed running/stopped cell to have proper aligment

--- src/views/pages/Services/ServicesIndex/CellStatus.tsx
+++ src/views/pages/Services/ServicesIndex/CellStatus.tsx
@@ -7,11 +7,17 @@ function CellStatus({ label, status }: { status: CellStatus, label: string }) {
 
     if(!label) return <></>
     return (
-        <td className={`whitespace-nowrap px-3 py-4 text-gray-500"`}>
-            <div className={`px-1.5 py-1 inline-block rounded-2xl space-x-2 w-full text-center ${mediaTypes[status]}`}>
+        <td className={`whitespace-nowrap px-3 py-4`}>
+        <div className="inline-block">
+
+            <div className={`px-2 rounded-2xl w-full flex items-center text-center ${mediaTypes[status]}`}>
+            <div className="py-2 flex items-center align-center space-x-2">
                 <div className={`inline-block h-2 w-2 rounded-full ${ status === "running" ? "bg-[#12B76A]" : "bg-[#F02849]"}`}></div>
-                <span className="inline-block capitalize font-medium text-xs">{label}</span>
+                <p className="block capitalize font-medium text-xs leading-3">{label}</p>
             </div>
+            </div>
+
+        </div>
         </td>
     );
 }
